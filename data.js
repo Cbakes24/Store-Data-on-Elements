77e3ee1a01b362bc4e4c9fb346ea30d246dfc481
geoffreyjiang <geoffreychjiang@gmail.com>
2022-07-26 19:36:18 -0700

    fin

--- data.js
+++ data.js
@@ -1,1 +1,14 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.getElementById("add");
+  const shoppingList = document.getElementById("shopping-list");
+
+  addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const li = document.createElement("li");
+    const type = document.getElementById("type");
+    const namePut = document.querySelector("#name");
+    li.setAttribute("data-type", type.value);
+    li.innerText = namePut.value;
+    shoppingList.appendChild(li);
+  });
+});
